5b6f821e37d6c1c76e9e3a7e4ef6b53a99a43d19
usstjmj <792677691@qq.com>
2016-12-16 23:22:16 +0800

    药房端

---
+++ Hospital/src/Medicine.java
@@ -0,0 +1,25 @@
+import java.awt.BorderLayout;
+import javax.swing.*;
+
+public class Medicine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		           JFrame frm=new JFrame("药师登录");
+		      		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		      		
+		      		JPanel top=new JPanel();
+		      		frm.getContentPane().add(top, BorderLayout.NORTH);
+		      		JLabel label=new JLabel("药房储存信息");
+		      		top.add(label);
+		      		
+		      		JPanel center=new JPanel();
+		      		frm.getContentPane().add(center, BorderLayout.CENTER);
+		      		JTextArea area=new JTextArea();
+		          }
+		
+	}
+
+
